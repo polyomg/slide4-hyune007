0bf9ab27bcbc52bc317c0ab71077d7e7c8f77457
HyuMorreti <huyhuyhuy201107@gmail.com>
2025-10-02 13:33:40 +0700

    TS00689 - Nguyen Truong Huy - Slide 4 - First Commit

---
+++ DemoSlide4/src/test/java/com/poly/controller/DemoSlide4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSlide4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
